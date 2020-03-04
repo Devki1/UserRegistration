ed5338baa7ec63ecae269ad8ea9317fa25227c8b
Devki1 <gdev3195@gmail.com>
2020-03-04 15:05:13 +0530

    ADDED:Check valid a last name

--- userRegistrationProblem.sh
+++ userRegistrationProblem.sh
@@ -1,10 +1,20 @@
 #!/bin/bash -x
 echo "Welcome to user registration problem"
 read -p "Enter the first name:" FirstName
+#Pattern for first name
 patternFirstName="^[A-Z][a-z]{2,}$"
 if [[ $FirstName =~ $patternFirstName ]]
 then
-		echo Valid
+	echo Valid
 else
-		echo Invalid
+	echo Invalid
+fi
+read -p "Enter the last name:" LastName
+#Pattern for last name
+patternLastName="^[A-Z]{1}[a-z]{2,}$"
+if [[ $LastName =~ $patternLastName ]]
+then
+	echo Valid
+else
+	echo Invalid
 fi
