f17a6323f6baf6791ce9e57c222391f639494bcd
Devki1 <gdev3195@gmail.com>
2020-03-04 16:00:28 +0530

    ADDED:feature of valid emailid

--- userRegistrationProblem.sh
+++ userRegistrationProblem.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -x
 echo "Welcome to user registration problem"
+function validFirstName()
+{
 read -p "Enter the first name:" FirstName
 #Pattern for first name
 patternFirstName="^[A-Z][a-z]{2,}$"
@@ -9,6 +11,9 @@ then
 else
 	echo Invalid
 fi
+}
+function validLastName()
+{
 read -p "Enter the last name:" LastName
 #Pattern for last name
 patternLastName="^[A-Z]{1}[a-z]{2,}$"
@@ -18,3 +23,19 @@ then
 else
 	echo Invalid
 fi
+}
+function validEmailId()
+{
+read -p "Enter a valid email id:" EmailId
+#Pattern for valid emailid 
+patternForEmailId="^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-z]{2,}$"
+if [[ $EmailId =~ $patternForEmailId ]]
+then
+	echo Valid
+else
+	echo Invalid
+fi
+}
+validFirstName
+validLastName
+validEmailId
